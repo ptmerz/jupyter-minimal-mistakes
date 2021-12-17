810008718e8a2e3f80f0157c5ea4845f88fe5b94
Pascal Merz <pascal.merz@me.com>
2021-12-17 14:02:28 +0100

    First version of action, including some debugging information

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Container image that runs our code.
+# base-notebook is a ubuntu image with a light-weight Jupyter installation
+FROM jupyter/base-notebook:2021-12-16
+
+# The action needs the special notebook layout and the action script
+# We'll just copy the entire folder there
+COPY . /action
+
+# Run the action script when starting the docker container
+ENTRYPOINT ["/action/convert_notebooks.sh"]
---
+++ convert_notebooks.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "DEBUG: pwd"
+pwd
+echo "DEBUG: ls -a -l"
+ls -a -l
+
+# Sanity checks for root directory
+if [ ! -d ./_posts ]; then
+  echo "Cannot find posts directory ./_posts" >&2
+  exit 1
+fi
+if [ ! -d ./_notebooks ]; then
+  echo "Cannot find notebook directory ./_notebooks" >&2
+  exit 1
+fi
+if [ ! -d ./assets/images/ ]; then
+  echo "Cannot find image directory ./assets/images/" >&2
+  exit 1
+fi
+root_directory=$PWD
+
+# Make sure that notebook format is available
+mkdir -p _layouts
+# If notebook.html exists, we don't copy it there. This allows users to have their own version
+# of the layout - at their own responsibility.
+if [ -e _layouts/notebook.html ]; then
+   echo "_layouts/notebook.html exists already, keeping the existing file"
+else
+   cp /action/_layouts/notebook.html _layouts/notebook.html
+fi
+
+cd _notebooks || exit 1
+for notebook_file in *.ipynb; do
+  # Get file name without extension
+  filename="${notebook_file%.*}"
+
+  echo "Converting $notebook_file ..."
+  # Name of Markdown file will be base name + ".md"
+  md_notebook="${filename}.md"
+  # Convert notebook to Markdown
+  jupyter nbconvert "$notebook_file" --to markdown
+  # Fix image links to point to a place where Jekyll will pick up the images
+  perl -pi -e 's/\!\[png\]\(/![png]({{ site.url }}{{ site.baseurl }}\/assets\/images\//g' "$md_notebook"
+  
+  # Check if there is a YAML front matter
+  if [ "$(sed -n '/^---$/p;q' "$md_notebook")" ]; then
+    perl -pi -e "s/---/---\nlayout: notebook\nfilename: \"$notebook_file\"/ if 1 .. 1" "$md_notebook"
+  else
+    perl -pi -e "s/^/---\nlayout: notebook\nfilename: \"$notebook_file\"\n---\n/ if 1 .. 1" "$md_notebook"
+  fi
+  
+  # Move Markdown notebook to _posts/ directory
+  rm -f "${root_directory}/_posts/${md_notebook}"
+  mv "$md_notebook" "${root_directory}/_posts/"
+  # Move images to a assets such that Jekyll finds them
+  rm -rf "${root_directory}/assets/images/${filename}_files"
+  mv "${filename}_files" "${root_directory}/assets/images/"
+done
+cd "$root_directory" || exit 1
